63a80471f06e08eaf0b130b2365ace61403a3e9c
Sam Myers <sam@students.olin.edu>
2017-04-16 18:35:49 -0400

    Created skeleton Meteor app.

---
+++ imports/ui/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">Welcome to the Feelings Cloud</div>
+        );
+    }
+}
