2ee579334c7de585d9da5876f58afd00d77c1775
Sam Myers <sam@students.olin.edu>
2017-04-16 21:58:56 -0400

    Add copyright footer.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
     render() {
+        const yearString = `Copyright © ${(new Date()).getFullYear()} by `;
+
         return (
-            <div className="app">Welcome to the Feelings Cloud</div>
+            <div className="app">
+                <div className="header">Welcome to the Feelings Cloud</div>
+                <div className="footer">
+                    <div className="copyright">
+                        {yearString}
+                        <a href="http://8byt.com">8byt Software</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
