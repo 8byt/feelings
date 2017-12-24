175092ee2ad9a8d4a31b755b9dec95b0491081d0
Sam Myers <sam@students.olin.edu>
2017-12-23 17:16:12 -0800

    Remove all Meteor.

--- client/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-export const createPost = emotion => {
-    return dispatch => Meteor.call('createPost', emotion);
-};
--- server/publications.js
+++
@@ -1,4 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import Posts from '../imports/api/posts';
-
-Meteor.publish('posts', () => Posts.find({}));
