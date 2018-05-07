55901f6f5939bb1648ec3c3fc88f0ef2f077f2dc
Sam Myers <sam@students.olin.edu>
2018-05-07 02:41:40 -0400

    please

--- webapp/src/common/utils/createEmoji.js
+++ webapp/src/common/utils/createEmoji.js
@@ -1,6 +1,6 @@
 import emoji from 'react-easy-emoji';
 
-export default function createEmoji(input) {
+export default function createEmoji(input = '?') {
   return emoji(input, {
     baseUrl: '//twemoji.maxcdn.com/2/svg',
     ext: '.svg',
